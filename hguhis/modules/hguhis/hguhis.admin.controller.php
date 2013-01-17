ae00207a04b6d75f8ffc0bad22970fddc5b9eef5
wndflwr <wndflwr@gmail.com>
2013-01-17 14:37:28 +0900

    작업 분담되었음.

--- hguhis/modules/hguhis/hguhis.admin.controller.php
+++ hguhis/modules/hguhis/hguhis.admin.controller.php
@@ -1,1 +1,24 @@
 <?php
+/**
+ * @class  hguhisAdminController
+ * @author CRA (cra.handong@gamil.com)
+ * hguhis module's admin controller class
+ **/
+
+class hguhisAdminController extends hguhis {
+	
+	function init() {
+		
+	}
+	
+	/**
+	 * @function procHguhisAdminModuleInsert
+	 * @brief 모듈 정보와 우리 위키 모듈만의 정보 받아서 저장한다.
+	 * modules, module_part_config
+	 * @author 지희
+	 */
+	function procHguhisAdminModuleInsert() {
+		
+	}
+}
+?>
--- hguhis/modules/hguhis/hguhis.admin.view.php
+++ hguhis/modules/hguhis/hguhis.admin.view.php
@@ -1,1 +1,33 @@
 <?php
+/**
+ * @class  hguhisAdminView
+ * @author CRA (cra.handong@gamil.com)
+ * hguhis module's admin view class
+ **/
+
+class hguhisAdminView extends hguhis {
+	
+	function init() {
+		
+	}
+	
+	/**
+	 * @function dispHguhisAdminModuleList
+	 * @brief 모듈들의 리스트가 보여진다.
+	 * @author 인호
+	 */
+	function dispHguhisAdminModuleList() {
+		
+	}
+	
+	/**
+	 * @function dispHguhisAdminModuleInsert
+	 * @brief 모듈의 정보를 삽입한다.
+	 * @author 현희
+	 */
+	function dispHguhisAdminModuleInsert() {
+		
+	}
+	
+}
+?>

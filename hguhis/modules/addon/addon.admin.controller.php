6d562672a325b0aa8c1a338785a6e380372c0c8b
wndflwr <wndflwr@gmail.com>
2013-01-18 11:58:50 +0900

    addon.admin.controller.php test.

--- hguhis/modules/addon/addon.admin.controller.php
+++ hguhis/modules/addon/addon.admin.controller.php
@@ -1,7 +1,7 @@
 <?php
     require_once(_XE_PATH_.'modules/addon/addon.controller.php');
 
-    /** Test. This line should not be ignored.
+    /** 테스트 해 봅니다.
      * Admin controller class of addon modules
      * @author NHN (developers@xpressengine.com)
      **/
